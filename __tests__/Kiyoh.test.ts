c52a1620716541b6dcc448ac2eea1e35c909ef5d
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-01-07 15:38:39 +0100

    Made readme
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

--- __tests__/Kiyoh.test.ts
+++ __tests__/Kiyoh.test.ts
@@ -1,8 +1,16 @@
 import { VMWKiyoh } from '../Kiyoh';
 let k = new VMWKiyoh();
 
-describe('Kiyoh base tests', () => {
-    test('Kiyoh', () => {
+describe('Kiyoh', () => {
+    test('is instance of VMWKiyoh', () => {
         expect(k).toBeInstanceOf(VMWKiyoh);
     });
 });
+
+describe('Kiyoh properties', () => {
+    test('Are undefined', () => {
+        expect(k.average).toBeUndefined();
+        expect(k.dataUrl).toBeUndefined();
+        expect(k.target).toBeUndefined();
+    })
+})
